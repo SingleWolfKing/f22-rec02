635067fa738eccb2c76a5a7e556c309471c67839
SingleWolfKing <97373784+SingleWolfKing@users.noreply.github.com>
2022-09-07 15:08:57 -0400

    update my hw

--- java/src/main/java/edu/cmu/cs/cs214/rec02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/rec02/Main.java
@@ -5,7 +5,7 @@ import edu.cmu.cs.cs214.rec02.points.*;
 public class Main {
     public static void main(String[] args) {
         PolarPoint point1 = new PolarPoint(2, 0);
-        PolarPoint point2 = new PolarPoint(4, Math.PI);
+        CartesianPoint point2 = new CartesianPoint(4, 6);
         
         Line line = new Line(point1, point2);
         
